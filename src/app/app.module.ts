5909941cc6498536f29d054d9665d82b2202d9ce
mhmtsrt11@gmail.com <44hm3ts3rt>
2019-05-24 17:55:29 +0300

    add universal - ssr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,7 @@ import {HttpClientModule} from '@angular/common/http';
     LoadingSpinnerComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'angularServerSideRendering' }),
     AppRoutingModule,
     HttpClientModule
   ],
