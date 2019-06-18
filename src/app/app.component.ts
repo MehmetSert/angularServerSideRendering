3ca3601db0c5d965c6daf48f2d84e00d570fc5ae
mhmtsrt11@gmail.com <44hm3ts3rt>
2019-06-18 16:59:37 +0300

    ssr localStorage-setTimeout... is not defined

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, Inject, OnInit, PLATFORM_ID} from '@angular/core';
+import {isPlatformBrowser} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angularServerSideRendering';
+
+  constructor(
+    @Inject(PLATFORM_ID) private _platformId: Object
+  ) { }
+
+  ngOnInit() {
+    if (isPlatformBrowser(this._platformId)) {
+      localStorage.setItem('token', 'xxxxxxxxxxxxxx');
+    }
+  }
+
 }
